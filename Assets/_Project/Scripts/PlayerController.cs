2bb2c0b8688ef0280a4c0980e78afb38d9bb1157
Nicolas <76026392@continental.edu.pe>
2026-09-06 13:38:53 -0500

    Añadida UI de monedas, trampa Spikes y barril físico empujable con Rigidbody2D

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -1,53 +1,88 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-// Movimiento y salto
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movimiento")]
     [SerializeField] private float speed = 2f;
     [SerializeField] private float jumpForce = 5.5f;
-    [SerializeField] private float groundRadius = 0.15f;
+
+    [Header("Detección Suelo")]
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundRadius = 0.15f;
     [SerializeField] private LayerMask groundLayer;
 
-    private Rigidbody2D rb;
+    [Header("UI")]
+    [SerializeField] private TMP_Text textCoins;
+
+    private Rigidbody2D rb2D;
+    private Animator animator;
     private float move;
     private bool isGrounded;
+    private int coins;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // 1. Input horizontal instantáneo (sin inercia)
+        // 1. Input horizontal y velocidad
         move = Input.GetAxisRaw("Horizontal");
+        rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
 
-        // 2. Giro de sprite izquierda/derecha
+        // 2. Giro visual
         if (move != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
         }
 
-        // 3. Chequeo de suelo
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
-
-        // 4. Salto (bloquea salto infinito)
+        // 3. Salto
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
+        }
+
+        // 4. Parámetros del Animator
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(move));
+            animator.SetFloat("VerticalVelocity", rb2D.linearVelocity.y);
+            animator.SetBool("IsGrounded", isGrounded);
         }
     }
 
     void FixedUpdate()
     {
-        // Físicas horizontales manteniendo caída natural
-        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+        // Chequeo físico de suelo
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 
-    void OnDrawGizmosSelected()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Guía visual en escena
+        // Monedas: suma y destruye
+        if (collision.transform.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+            coins++;
+            if (textCoins != null)
+            {
+                textCoins.text = coins.ToString();
+            }
+        }
+
+        // Trampa: reinicia nivel
+        if (collision.transform.CompareTag("Spikes"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
         if (groundCheck != null)
         {
             Gizmos.color = Color.green;
